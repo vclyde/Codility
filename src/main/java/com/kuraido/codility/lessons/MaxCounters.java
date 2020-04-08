51848ba4926f015fbbe9c90e41ec9e9050ff42ae
Clyde Velasquez <clydevelasquez@outlook.com>
2020-04-09 01:30:07 +0800

    Optimize solution

--- src/main/java/com/kuraido/codility/lessons/MaxCounters.java
+++ src/main/java/com/kuraido/codility/lessons/MaxCounters.java
@@ -1,7 +1,5 @@
 package com.kuraido.codility.lessons;
 
-import java.util.Arrays;
-
 /**
  *
  * @author cvelasquez
@@ -15,18 +13,28 @@ public class MaxCounters {
 		// Constraints
 		if ((N > 0 && N <= 100000) && (m > 0 && m <= 100000)) {
 			int max = 0;
+			int start = 0;
 			for (int K = 0; K < m; K++) {
 				// Ensure that each value of the array must be within the range of 1 to N+1
 				if (A[K] > 0 && A[K] <= (N + 1)) {
 					if (A[K] <= N) {
+						if (counter[A[K] - 1] < start) {
+							counter[A[K] - 1] = start;
+						}
 						counter[A[K] - 1]++;
 						if (counter[A[K] - 1] > max) {
 							max = counter[A[K] - 1];
 						}
 					} else if (A[K] == (N + 1)) {
-						
-						Arrays.fill(counter, max);
+						start = max;
 					}
+				}
+			}
+
+			// Set all remaning
+			for (int i = 0; i < N; i++) {
+				if (counter[i] < start) {
+					counter[i] = start;
 				}
 			}
 		}
