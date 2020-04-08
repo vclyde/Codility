28ca2c355012e1fcae1f4979218ae84de67a0da7
Clyde Velasquez <clydevelasquez@outlook.com>
2020-04-08 15:53:14 +0800

    Needs optimization

--- src/main/java/com/kuraido/codility/lessons/MaxCounters.java
+++ src/main/java/com/kuraido/codility/lessons/MaxCounters.java
@@ -1,5 +1,7 @@
 package com.kuraido.codility.lessons;
 
+import java.util.Arrays;
+
 /**
  *
  * @author cvelasquez
@@ -9,30 +11,25 @@ public class MaxCounters {
 	public static int[] solution(int N, int[] A) {
 		int m = A.length;
 		int[] counter = new int[N];
-		int max = 0;
 
-		if ((N > 0 && N <= 100000) && (m > 0 && m <= 100000)) { // Constraint
-			for (int i = 0; i < m; i++) {
-
-				if (A[i] > 0 && A[i] <= N) {
-					counter[A[i] - 1]++;
-					if (counter[A[i - 1]] > max) {
-						max = counter[A[i] - 1];
+		// Constraints
+		if ((N > 0 && N <= 100000) && (m > 0 && m <= 100000)) {
+			int max = 0;
+			for (int K = 0; K < m; K++) {
+				// Ensure that each value of the array must be within the range of 1 to N+1
+				if (A[K] > 0 && A[K] <= (N + 1)) {
+					if (A[K] <= N) {
+						counter[A[K] - 1]++;
+						if (counter[A[K] - 1] > max) {
+							max = counter[A[K] - 1];
+						}
+					} else if (A[K] == (N + 1)) {
+						
+						Arrays.fill(counter, max);
 					}
 				}
-
-//				if (A[i] > 0 && A[i] <= N) {
-//					counter[A[i] - 1]++;
-//					if (counter[A[i] - 1] > max) {
-//						max = counter[A[i] - 1];
-//					}
-//				} else if (A[i] == (N + 1)) {
-//					// Needs optimization
-//					Arrays.fill(counter, max);
-//				}
 			}
 		}
-
 		return counter;
 	}
 }
