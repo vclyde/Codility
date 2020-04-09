1378e12559553c2f2dc82fa3d5d6dba8620b5e52
Clyde Velasquez <clydevelasquez@outlook.com>
2020-04-09 17:15:14 +0800

    Solve MissingElement problem

---
+++ src/main/java/com/kuraido/codility/lessons/MissingElement.java
@@ -0,0 +1,32 @@
+package com.kuraido.codility.lessons;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Clyde Velasquez
+ */
+public class MissingElement {
+	
+	public static int solution(int[] A) {
+		int missing = 0;
+		int[] counter = new int[1000001];
+		
+		if (A.length > 0 && A.length <= 100000) {
+			for (int i = 0; i < A.length; i++) {
+				if (A[i] > 0 && A[i] < 1000000) {
+					counter[A[i]]++;
+				}
+			}
+			
+			for (int i = 1; i < counter.length; i++) {
+				if (counter[i] == 0) {
+					missing = i;
+					break;
+				}
+			}
+		}
+		
+		return missing;
+	}
+}
